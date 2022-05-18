9a3bd9c186a3e824e195eb4e50a5dcdbf2f10917
Lucas Ferreira <lucasfpnt@gmail.com>
2022-05-18 16:54:04 -0300

    requisito 4

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 import '../styles/Header.scss';
@@ -24,6 +25,17 @@ const Header = () => {
         <header className="header-component">
           <section className="header-component__user">
             <h2 data-testid="header-user-name">{user}</h2>
+          </section>
+          <section className="header-component__links">
+            <Link to="/search" className="link" data-testid="link-to-search">
+              Search
+            </Link>
+            <Link to="/favorites" className="link" data-testid="link-to-favorites">
+              Favorites
+            </Link>
+            <Link to="/profile" className="link" data-testid="link-to-profile">
+              Profile
+            </Link>
           </section>
         </header>
       )}
