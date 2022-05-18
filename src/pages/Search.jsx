e8237c2557d36ece88ba776ae250183f01e36ad7
Lucas Ferreira <lucasfpnt@gmail.com>
2022-05-18 18:16:28 -0300

    requisito 6

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,21 +1,30 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Loading from '../components/Loading';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 
 const Search = () => {
   const [artista, setArtista] = useState('');
+  const [artistaSave, setArtistaSave] = useState('');
+  const [mostraMusica, setMostraMusica] = useState(false);
+  const [listaDeMusica, setListaDeMusica] = useState([]);
   const [disabled, setDisabled] = useState(true);
+  const [loading, setLoading] = useState(false);
 
   const HandleChange = (event) => {
     setArtista(event.target.value);
+    setArtistaSave(event.target.value);
   };
 
-  const Logar = async (event) => {
-    setLoading(true);
+  const Procurar = async (event) => {
     event.preventDefault();
-    await createUser({ name: nome });
+    setLoading(true);
+
+    setListaDeMusica(await searchAlbumsAPI(artista));
     setLoading(false);
-    setLogged(true);
+    setMostraMusica(true);
+    setArtista('');
   };
 
   useEffect(() => {
@@ -30,22 +39,70 @@ const Search = () => {
   return (
     <>
       <Header />
-      <form>
-        <input
-          type="text"
-          data-testid="search-artist-input"
-          value={ artista }
-          onChange={ HandleChange }
-        />
-        <button
-          type="submit"
-          data-testid="search-artist-button"
-          onClick={ Logar }
-          disabled={ disabled }
-        >
-          Procurar
-        </button>
-      </form>
+      {loading ? (
+        <Loading />
+      ) : (
+        <>
+          <form>
+            <input
+              type="text"
+              data-testid="search-artist-input"
+              value={ artista }
+              onChange={ HandleChange }
+            />
+            <button
+              type="submit"
+              data-testid="search-artist-button"
+              onClick={ Procurar }
+              disabled={ disabled }
+            >
+              Procurar
+            </button>
+          </form>
+          {mostraMusica && (
+            <div>
+              <h1>
+                { `Resultado de álbuns de: ${artistaSave}`}
+              </h1>
+              {listaDeMusica.map((musica) => (
+                <Link
+                  to={ `/album/${musica.collectionId}` }
+                  data-testid={ `link-to-album-${musica.collectionId}` }
+                  key={ musica.artistId }
+                >
+                  <li>
+                    <img
+                      src={ musica.artworkUrl100 }
+                      alt={ musica.collectionName }
+                    />
+                    <h2>
+                      Artista:
+                      {' '}
+                      {musica.artistName}
+                    </h2>
+                    <h2>
+                      ID:
+                      {' '}
+                      {musica.artistId}
+                    </h2>
+                    <h3>
+                      Album:
+                      {' '}
+                      {musica.collectionName}
+                    </h3>
+                    <h3>
+                      Preço:
+                      {' '}
+                      {musica.collectionPrice}
+                    </h3>
+                  </li>
+                </Link>
+              ))}
+              {listaDeMusica.length === 0 && <h1>Nenhum álbum foi encontrado</h1>}
+            </div>
+          )}
+        </>
+      )}
     </>
   );
 };
